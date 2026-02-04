a5f2e2e1703a97bbf9f929a3dc581248effc63cc
Kostas <hello@kostaskova.lt>
2026-02-04 10:52:02 +0200

    Pridetas hub.py nepaisant gitignore

---
+++ hub.py
@@ -0,0 +1,6 @@
+def patikrinti_prisijungima():
+    # Spausdiname pranesima be lietuvisku raidziu
+    print("Sveikas! Jei matai sita teksta, GitHub veikia.")
+
+if __name__ == "__main__":
+    patikrinti_prisijungima()
